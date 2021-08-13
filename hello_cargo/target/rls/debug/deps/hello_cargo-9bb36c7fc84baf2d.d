db5b1eae0f5821177c0305f724717db218b42e04
Egor Zhilinskiy <sheg.bis@gmail.com>
2021-08-13 19:13:38 +0700

    make prj
    make file

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-9bb36c7fc84baf2d.d
@@ -0,0 +1,5 @@
+/home/Sheguls_Proum/Rust_Learning/hello_cargo/target/rls/debug/deps/hello_cargo-9bb36c7fc84baf2d.rmeta: src/main.rs
+
+/home/Sheguls_Proum/Rust_Learning/hello_cargo/target/rls/debug/deps/hello_cargo-9bb36c7fc84baf2d.d: src/main.rs
+
+src/main.rs:
